45cd9c501a1854ec39189b4a0e5ecc7d01da9e89
Clinton Ayres <clinton.ayres@theice.com>
2015-01-23 12:42:27 -0500

    -removed imple of autolabel

--- ValidationMixin.js
+++ ValidationMixin.js
@@ -4,26 +4,11 @@
 var ValidationFactory = require('./ValidationFactory');
 
 var ValidationMixin = {
-  // autoLabel: [<String>] | Boolean
 
   validate: function() {
     var validatorTypes = this.validatorTypes || {};
     if (typeof this.validatorTypes === 'function') {
       validatorTypes = this.validatorTypes();
-    }
-
-    // Call `.label` method on values of `validatorTypes` to make error messages human readable
-    var autoLabel = [];
-    if (Array.isArray(this.autoLabel)) {
-      autoLabel = this.autoLabel;
-    }
-    else if (this.autoLabel === true) {
-      autoLabel = Object.keys(validatorTypes);
-    }
-    if (autoLabel.length) {
-      autoLabel.forEach(function(key) {
-        validatorTypes[key] = validatorTypes[key].label(this.refs[key].props.label);
-      }.bind(this));
     }
 
     return ValidationFactory.validate(validatorTypes, this.state);
