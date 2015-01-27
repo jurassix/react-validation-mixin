f8909da5ffd64d0d8efdc476078e40318c123e1c
Ivan Kleshnin <ivan@paqmind.com>
2015-01-27 14:43:06 +0200

    Update tests to new behavior. Remove unnecessary tests

--- spec/ValidationFactorySpec.js
+++ spec/ValidationFactorySpec.js
@@ -1,366 +1,188 @@
-var chai = require('chai');
-var should = chai.should();
+var expect = require('chai').expect;
 var Joi = require('joi');
 
 var ValidationFactory = require('../ValidationFactory');
 
 describe('Validation Factory', function() {
-
-  before(function() {
-
-    this.validationTypes = {
-      username: Joi.string().alphanum().min(3).max(30).required().label('Username'),
-      password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/)
-    };
-
-    this.refValidationTypes = {
-      username: Joi.string().alphanum().min(3).max(30).required().label('Username'),
-      password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).label('Password'),
-      verifyPassword: Joi.ref('password')
-    };
-
-    this.state = {
-      username: 'bar',
-      password: 'ab1'
-    };
-
-    this.invalidState = {
-      username: 'a_',
-      password: 'a_'
-    };
-
-    this.additionalState = {
-      foo: 'bar'
-    };
-
-  });
-
   describe('validation()', function() {
-
-    describe('valid scenarios', function() {
-
-      it('should return no errors for valid input', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.state
-        };
-
-        var result = ValidationFactory.validate(options);
-        chai.expect(result['username']).to.be.undefined;
-        chai.expect(result['password']).to.be.undefined;
-
+    describe('edge cases', function() {
+      it('should follow `schema` keys', function() {
+        var result1 = ValidationFactory.validate({}, undefined);
+        expect(result1).to.eql({});
+        var result2 = ValidationFactory.validate({username: Joi.string()}, undefined);
+        expect(result2).to.eql({username: []});
       });
 
-      it('should return no errors for undefined input', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.additionalState
-        };
-
-        var result = ValidationFactory.validate(options);
-        chai.expect(result['foo']).to.be.undefined;
-
-      });
-
-      it('should use labels from Joi Schema when creating error messages', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: {}
-        };
-
-        var result = ValidationFactory.validate(options);
-        JSON.stringify(result['username']).should.equal('["Username is required"]');
-
-      });
-
-      describe('should only validate a specified field', function() {
-
-        it('when field is valid and other fields are invalid', function() {
-
-          var options = {
-            schema: this.validationTypes,
-            state: {},
-            field: 'password'
-          };
-
-          var result = ValidationFactory.validate(options);
-          chai.expect(result['username']).to.be.undefined;
-          chai.expect(result['password']).to.be.undefined;
-
-        });
-
-        it('when field is invalid', function() {
-
-          var options = {
-            schema: this.validationTypes,
-            state: {},
-            field: 'username'
-          };
-
-          var result = ValidationFactory.validate(options);
-          JSON.stringify(result['username']).should.equal('["Username is required"]');
-          chai.expect(result['password']).to.be.undefined;
-
-        });
-
-        it('when field is defined', function() {
-
-          var options = {
-            schema: this.validationTypes,
-            state: {},
-            field: undefined
-          };
-
-          var result = ValidationFactory.validate(options);
-          JSON.stringify(result['username']).should.equal('["Username is required"]');
-          chai.expect(result['password']).to.be.undefined;
-
-        });
-
-        it('when field is a ref include the reference', function() {
-
-          var options = {
-            schema: this.refValidationTypes,
-            state: {
-              username: 'anoyn',
-              password: '1234abcd',
-              verifyPassword: null
-            },
-            field: 'verifyPassword'
-          };
-
-          var result = ValidationFactory.validate(options);
-          JSON.stringify(result['verifyPassword']).should.equal('["verifyPassword must be one of ref:password"]');
-          chai.expect(result['password']).to.be.undefined;
-
-        });
+      it('should follow `data` keys', function() {
+        var result1 = ValidationFactory.validate(undefined, {});
+        expect(result1).to.eql({});
+        var result2 = ValidationFactory.validate(undefined, {username: 'foo'});
+        expect(result2).to.eql({username: []});
       });
     });
 
-    describe('invalid scenarios', function() {
-
-      it('should throw exception when validation types is undefined', function() {
-
-        var options = {
-          schema: undefined,
-          state: this.state
+    describe('of entire form', function() {
+      it('should handle mix of valid, invalid and undefined inputs', function() {
+        var schema = {
+          username: Joi.string().required(),
+          age: Joi.number(),
+          bonus: Joi.boolean(), // data is undefined
         };
-
-        chai.expect(function() {
-          ValidationFactory.validate(options);
-        }).to.throw('schema is undefined');
-
+        var data = {
+          username: '', // invalid
+          password: 'qwerty', // valid required
+          age: 10, // valid optional
+          something: "xyz" // schema is undefined
+        };
+        var result = ValidationFactory.validate(schema, data);
+        expect(result).to.eql({
+          username: ["username is not allowed to be empty"],
+          password: [],
+          age: [],
+          bonus: [],
+          something: [],
+        });
       });
 
-      it('should not throw exception when state is undefined', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: undefined
+      it('should return multiple errors for multiple failed validations', function() {
+        var schema = {
+          password: Joi.string().alphanum().min(6),
         };
-
-        var result = ValidationFactory.validate(options);
-        result['username'].should.not.be.empty;
-        JSON.stringify(result['username']).should.equal('["Username is required"]');
-
+        var data = {
+          password: '???',
+        };
+        var result = ValidationFactory.validate(schema, data);
+        expect(result['password']).to.eql([
+          "password must only contain alpha-numeric characters",
+          "password length must be at least 6 characters long"
+        ]);
       });
 
-      it('should validate field and not throw exception when state is undefined', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: undefined,
-          field: 'username'
+      it('should use labels from Joi Schema', function() {
+        var schema = {
+          username: Joi.string().alphanum().min(3).max(30).required().label('Username'),
+          password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/)
         };
+        var data = {};
+        var result = ValidationFactory.validate(schema, data);
 
-        var result = ValidationFactory.validate(options);
-        result['username'].should.not.be.empty;
-        JSON.stringify(result['username']).should.equal('["Username is required"]');
-
-      });
-
-      it('should return multiple errors for input failing multiple validations', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.invalidState
-        };
-
-        var result = ValidationFactory.validate(options);
-        result['username'].should.not.be.empty;
-        JSON.stringify(result['username']).should.equal('["Username must only contain alpha-numeric characters","Username length must be at least 3 characters long"]');
-
-      });
-
-      it('should return single error for input failing single validations', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.invalidState
-        };
-
-        var result = ValidationFactory.validate(options);
-        result['password'].should.not.be.empty;
-        JSON.stringify(result['password']).should.equal('["password fails to match the required pattern"]');
-
-      });
-    });
-  });
-
-  describe('isValid()', function() {
-
-    describe('valid scenarios', function() {
-
-      it('should be true if input field is valid', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.state
-        };
-
-        var validation = ValidationFactory.validate(options);
-        var result = ValidationFactory.isValid(validation, 'username');
-        result.should.be.true;
-
-      });
-
-      it('should be valid when fieldName not found in validation', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.state
-        };
-
-        var validation = ValidationFactory.validate(options);
-        var result = ValidationFactory.isValid(validation, 'unknown');
-        result.should.be.true;
-
-      });
-
-      it('should be true when all input is valid', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.state
-        };
-
-        var validation = ValidationFactory.validate(options);
-        var result = ValidationFactory.isValid(validation);
-        result.should.be.true;
-
-      });
-
-      it('should return true when validations is undefined', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.invalidState
-        };
-
-        var result = ValidationFactory.isValid();
-        result.should.be.true;
-
+        expect(result['username']).to.eql(["Username is required"]);
       });
     });
 
-    describe('invalid scenarios', function() {
-
-      it('should be invalid when input field fails validation', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.invalidState
+    describe('of specified key', function() {
+      it('should validate specified', function() {
+        var schema = {
+          username: Joi.string().required(),
         };
+        var data = {};
 
-        var validation = ValidationFactory.validate(options);
-        var result = ValidationFactory.isValid(validation, 'password');
-        result.should.be.false;
-
+        var result = ValidationFactory.validate(schema, data, "username");
+        expect(result.length).to.equal(1);
       });
 
-      it('should be invalid when input field fails multiple validations', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.invalidState
+      it('should not validate others', function() {
+        // TODO: see https://github.com/hapijs/joi/pull/484
+        var schema = {
+          username: Joi.string().required(),
+          password: Joi.string().required(),
         };
+        var data = {password: 'qwerty'};
 
-        var validation = ValidationFactory.validate(options);
-        var result = ValidationFactory.isValid(validation, 'username');
-        result.should.be.false;
-
+        var result = ValidationFactory.validate(schema, data, "password");
+        expect(result.length).to.equal(0);
       });
 
-      it('should be invalid when all input is any input is invalid', function() {
-
-        var options = {
-          schema: this.validationTypes,
-          state: this.invalidState
+      it('should handle Joi refs', function() {
+        // TODO: see https://github.com/hapijs/joi/pull/484
+        var schema = {
+          password: Joi.string().required(),
+          verifyPassword: Joi.any().valid(Joi.ref('password')).options({language: {any: {allowOnly: "don't match password"}}}).required()
+        };
+        var data = {
+          password: 'qwerty',
+          verifyPassword: 'qerty',
         };
 
-        var validation = ValidationFactory.validate(options);
-        var result = ValidationFactory.isValid(validation);
-        result.should.be.false;
-
+        var result3 = ValidationFactory.validate(schema, data, "verifyPassword");
+        expect(result3).to.eql(["verifyPassword don\'t match password"]);
       });
     });
   });
 
   describe('getValidationMessages()', function() {
+    describe('key is defined', function() {
+      it('should be empty for valid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {username: 'bar'};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.getValidationMessages(errors, 'username');
+        expect(result.length).to.equal(0);
+      });
 
-    it('should return multiple error messages for fieldName', function() {
-
-      var options = {
-        schema: this.validationTypes,
-        state: this.invalidState
-      };
-
-      var validation = ValidationFactory.validate(options);
-      var result = ValidationFactory.getValidationMessages(validation, 'username');
-      JSON.stringify(result).should.equal('["Username must only contain alpha-numeric characters","Username length must be at least 3 characters long"]');
-
+      it('should be filled for invalid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.getValidationMessages(errors, 'username');
+        expect(result.length).to.equal(1);
+      });
     });
 
-    it('should return single error message for fieldName', function() {
+    describe('key is undefined', function() {
+      it('should be empty for valid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {username: 'bar'};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.getValidationMessages(errors);
+        expect(result.length).to.equal(0);
+      });
 
-      var options = {
-        schema: this.validationTypes,
-        state: this.invalidState
-      };
+      it('should be filled for invalid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.getValidationMessages(errors);
+        expect(result.length).to.equal(1);
+      });
+    });
+  });
 
-      var validation = ValidationFactory.validate(options);
-      var result = ValidationFactory.getValidationMessages(validation, 'password');
-      JSON.stringify(result).should.equal('["password fails to match the required pattern"]');
+  describe('isValid()', function() {
+    describe('key is defined', function() {
+      it('should be true for valid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {username: 'bar'};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.isValid(errors, 'username');
+        expect(result).to.be.true;
+      });
 
+      it('should be false for invalid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.isValid(errors, 'username');
+        expect(result).to.be.false;
+      });
     });
 
-    it('should return empty array when validations is undefined', function() {
+    describe('key is undefined', function() {
+      it('should be true for valid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {username: 'bar'};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.isValid(errors);
+        expect(result).to.be.true;
+      });
 
-      var options = {
-        schema: this.validationTypes,
-        state: this.invalidState
-      };
-
-      var result = ValidationFactory.getValidationMessages(undefined, 'password');
-      result.should.be.empty;
-
-    });
-
-    it('should return all messages when no fieldName is provided', function() {
-
-      var options = {
-        schema: this.validationTypes,
-        state: this.invalidState
-      };
-
-      var validation = ValidationFactory.validate(options);
-      var result = ValidationFactory.getValidationMessages(validation);
-      result.length.should.equal(3);
-      JSON.stringify(result).should.equal('["Username must only contain alpha-numeric characters","Username length must be at least 3 characters long","password fails to match the required pattern"]');
-
+      it('should be false for invalid input', function() {
+        var schema = {username: Joi.string().required()};
+        var data = {};
+        var errors = ValidationFactory.validate(schema, data);
+        var result = ValidationFactory.isValid(errors);
+        expect(result).to.be.false;
+      });
     });
   });
 });
