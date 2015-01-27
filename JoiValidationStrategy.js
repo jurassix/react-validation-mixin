febb8fe9d7f547d587f1ef36fb779e7e0894aff8
Ivan Kleshnin <ivan@paqmind.com>
2015-01-27 13:22:56 +0200

    Refactor `JoiValidationStrategy`

--- JoiValidationStrategy.js
+++ JoiValidationStrategy.js
@@ -1,43 +1,39 @@
 var Joi = require('joi');
 
-var formatErrors = function(result) {
-  if (result.error !== null) {
-    return result.error.details.reduce(function(memo, detail) {
-      if (!Array.isArray(memo[detail.path])) {
-        memo[detail.path] = [];
-      }
-      memo[detail.path].push(detail.message);
-      return memo;
-    }, {});
-  } else {
-    return {};
-  }
-};
-
-var joiValidation = function(state, schema) {
-  return Joi.validate(state, schema, {
-    abortEarly: false,
-    allowUnknown: true,
-  });
-};
-
 var JoiValidationStrategy = {
-
-  validate: function(options) {
-    if (options && options.schema) {
-      var errors = formatErrors(joiValidation(options.state || {}, options.schema));
-      if (options.field) {
-        var result = {};
-        result[options.field] = errors[options.field];
-        return result;
-      } else {
-        return errors;
-      }
-
+  validate: function(joiSchema, data, key) {
+    joiSchema = joiSchema === undefined ? {} : joiSchema;
+    data = data === undefined ? {} : data;
+    var errors = this.format(Joi.validate(data, joiSchema, {
+      abortEarly: false,
+      allowUnknown: true,
+    }));
+    if (key === undefined) {
+      Object.keys(joiSchema).forEach(function(key) {
+        errors[key] = errors[key] || [];
+      });
+      Object.keys(data).forEach(function(key) {
+        errors[key] = errors[key] || [];
+      });
+      return errors;
+    } else {
+      return errors[key] || [];
     }
-    throw new Error('schema is undefined');
-  }
+  },
 
+  format: function(joiResult) {
+    if (joiResult.error !== null) {
+      return joiResult.error.details.reduce(function(memo, detail) {
+        if (!Array.isArray(memo[detail.path])) {
+          memo[detail.path] = [];
+        }
+        memo[detail.path].push(detail.message);
+        return memo;
+      }, {});
+    } else {
+      return {};
+    }
+  }
 };
 
 module.exports = JoiValidationStrategy;
